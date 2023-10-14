a13d92b4785abb200d505884e3dd4ed8f37f5bb6
nicholaishaw@gmail.com <nicholaishaw@gmail.com>
2023-10-14 11:55:01 -0400

    adding flask app

--- Surfsup/Starter_Code/app.py
+++ Surfsup/Starter_Code/app.py
@@ -1,5 +1,12 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+import pandas as pd
+import datetime as dt
 
 
 #################################################
@@ -8,23 +15,95 @@
 
 
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def homepage():
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/yyyy-mm-dd<br/>"
+        f"/api/v1.0/temp/yyyy-mm-dd/yyyy-mm-dd<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    print("Query Date: ", query_date)
+
+    # Perform a query to retrieve the data and precipitation scores
+    sql_variable = session.query(measurement.date, measurement.prcp).\
+    filter(measurement.date >= query_date).\
+    order_by(measurement.date).all()
+    precip = {}
+
+    for date, prcp in sql_variable:
+         precip[date]=prcp
+
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Design a query to calculate the total number of stations in the dataset
+    station_all = session.query(station.station).all()
+    session.close()
+
+    station_list = list(np.ravel(station_all))
+    return jsonify(stations = station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    tobs_all = session.query(measurement.tobs).filter(measurement.station == "USC00519281").filter(measurement.date >= query_date).all()
+    temp_list = list(np.ravel(tobs_all))
+    return jsonify(tobs = temp_list)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
+    
+    if not end:
+        results = session.query(*sel).filter(measurement.date >= start).all()
+
+        session.close()
+
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    else:
+        results = session.query(*sel).filter(measurement.date >= start).filter(measurement.date <= end).all()
+
+        session.close()
+
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+if __name__ == "__main__":
+    app.run(debug=True)
